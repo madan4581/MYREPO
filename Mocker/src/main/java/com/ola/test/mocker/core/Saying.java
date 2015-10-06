689de0e54c29ad4c06721d81baf0cc6b1e2d98f9
madan <madan.venugopal@gmail.com>
2015-10-06 15:06:31 +0530

    Testing git checking for mocker

---
+++ Mocker/src/main/java/com/ola/test/mocker/core/Saying.java
@@ -0,0 +1,38 @@
+package com.ola.test.mocker.core;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
+
+public class Saying {
+    private long id;
+    private String test;
+
+    @Length(max = 3)
+    private String content;
+
+    public Saying() {
+        // Jackson deserialization
+    }
+
+    public Saying(long id, String content) {
+        this.id = id;
+        this.content = content;
+        this.test="test";
+    }
+
+    @JsonProperty
+    public long getId() {
+        return id;
+    }
+
+    @JsonProperty
+    public String getContent() {
+        return content;
+    }
+    
+    @JsonProperty
+    public String getTest() {
+        return "This is test";
+    }
+    
+}
